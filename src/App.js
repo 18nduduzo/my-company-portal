0f1f044eeeae8f2439d1806ee3409fb5903c2b79
Nduduzo <nduduzohlophe84@gmail.com>
2026-02-19 15:02:56 +0200

    Initial commit with logo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/logo.png" alt="Company Logo" style={{ height: '120px', margin: '20px 0' }} />
+        <h1>Welcome to Contratech Civils Portal</h1>
+        <p>Log in to download your payslips</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
